17bf93fb81c7ce1d67dae8bf4eb77c2324ba8220
Tiago Acciaioli <tiagoacciaioli1@gmail.com>
2023-06-22 22:50:20 +0100

    back end new/index item

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,28 @@
+class ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+
+    if @item.save
+      redirect_to items_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :brand, :weight, :size, :year, :description, :price )
+  end
+
+end
