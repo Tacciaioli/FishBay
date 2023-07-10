66b2993d50e1d092dff589f4bcce9766a8b92596
Tiago Acciaioli <tiagoacciaioli1@gmail.com>
2023-07-10 22:28:18 +0100

    user first name, last name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo, :phone_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo, :phone_number, :first_name, :last_name])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:photo, :phone_number])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:photo, :phone_number, :first_name, :last_name])
   end
 end
